da50c6bbe0633becf0eac76962eba53d407149a5
Brenna Eckel <beckel@csumb.edu>
2017-05-09 16:20:55 -0700

    Update slot_machine.java

--- slot_machine.java
+++ slot_machine.java
@@ -7,21 +7,31 @@ public class Assign2
    public static void main(String[] args)
    {
       int toBet = 1;
-      int multiplier = 1;
-      TripleString pullString;
-      
+      int multiplier;
+      int winnings;
+      TripleString pullString = new TripleString();
+
       System.out.print("Welcome to the Hunger Games Slot Machine!\n");
       
-      while (toBet != 0)
+
+      while (toBet != 0 && pullString.continuePlay())
       {
          toBet = getBet();
-         pullString = pull();
-         multiplier = getPayMultiplier(pullString);
-         display(pullString, toBet*multiplier);
+         if(toBet == 0) break;
+        
+            pullString = pull();
+            multiplier = getPayMultiplier(pullString);
+            winnings = multiplier*toBet;
+            display(pullString, winnings);
+            pullString.saveWinnings(winnings);
+         
       } 
 
+      System.out.print(pullString.displayWinnings());
+      System.out.print("Thank you for playing.");
+
    }
-   
+
    public static int getBet()
    {
       //Scanner will allow for user input
@@ -33,12 +43,12 @@ public class Assign2
       {
          //Output welcome message
          System.out.print("Place a bet from 1 to 100 or enter 0 to quit: ");
-    
+
          //nextInt scans the next token of the input as an int. 
          bet = enterBet.nextInt();
-         
+
          //If statement returns bet range from 1 to 100
-         if (bet <= 100 && bet >= 1) 
+         if (bet <= 100 && bet >= 0) 
          {
             return bet;
          }
@@ -49,11 +59,11 @@ public class Assign2
    {
       //Use reel to set TripleString default constructor to initialize string members 
       TripleString reel = new TripleString();
-      
+
       //Output result message
       System.out.print("Thank you, and may the odds be ever in your favor!\n");
       System.out.print("***YOUR PULL IS***\n");
-      
+
       //Set mutators that intake private helper method randString()
       reel.setFirst(randString());
       reel.setSecond(randString());
@@ -64,13 +74,13 @@ public class Assign2
 
    static String randString()
    {
-      
+
       String randStr;
-      
+
       //generate a random integer in the range from 0 to 999
       Random randomGenerator = new Random(); 
       int randInt = randomGenerator.nextInt(1000);    
-      
+
       //assign string value to random number according to probabilities: 
       //50% "BAR", 25% "cherries", 12.5% "space", 12.5% "7"
       if (randInt < 500)
@@ -89,7 +99,7 @@ public class Assign2
       {
          randStr = "7";
       }
-      
+
       return randStr;  
 
    }
@@ -98,32 +108,32 @@ public class Assign2
    {
       // Sort the first 3rd of the TripleString
       switch (pullString.getFirst()) {
-         // If cherries is first
-         case "cherries":
-            // If the second 3rd of the TripleString does not equal cherries
-            if ("cherries".equals(pullString.getSecond())) {
-               return 5;
+      // If cherries is first
+      case "cherries":
+         // If the second 3rd of the TripleString does not equal cherries
+         if ("cherries".equals(pullString.getSecond())) {
+            return 5;
+         }
+         else if ("cherries".equals(pullString.getSecond())) {
+            if ("cherries".equals(pullString.getThird())) {
+               return 15;
             }
-            else if ("cherries".equals(pullString.getSecond())) {
-               if ("cherries".equals(pullString.getThird())) {
-                  return 15;
-               }
-               else if ("cherries".equals(pullString.getThird())) {
-                  return 30;
-               }
-            }  break;
+            else if ("cherries".equals(pullString.getThird())) {
+               return 30;
+            }
+         }  break;
          // If there are three BARs in a row
-         case "BAR":
-            if ("BAR".equals(pullString.getSecond())) {
-               if ("BAR".equals(pullString.getThird())) {
-                  return 50;
+      case "BAR":
+         if ("BAR".equals(pullString.getSecond())) {
+            if ("BAR".equals(pullString.getThird())) {
+               return 50;
             }
-            }  break;
+         }  break;
          // If there are three 7s in a row
-         case "7":
-            if ("7".equals(pullString.getSecond())) {
-               if ("7".equals(pullString.getThird())) {
-                  return 100;
+      case "7":
+         if ("7".equals(pullString.getSecond())) {
+            if ("7".equals(pullString.getThird())) {
+               return 100;
             }
          }  break;
       }
@@ -131,16 +141,15 @@ public class Assign2
       return 0;
    }
 
-// This method takes the winnings (a dollar amount) and thePull as parameters and displays the three strings inside thePull along with " losing statement" or "winning statement & dollar amount".
 
-
+   // This method takes the winnings (a dollar amount) and thePull as parameters and displays the three strings inside thePull along with " losing statement" or "winning statement & dollar amount".
    public static void display (TripleString thePull, int winnings) {
-       System.out.print (thePull.getFirst() + " " + thePull.getSecond() + " " + thePull.getThird() + "\n");
-       
-  if (winnings == 0)
-          System.out.println ("Losing statement… ");
-       else
-          System.out.println ("Winning statement: " + winnings);
+      System.out.print (thePull.getFirst() + " " + thePull.getSecond() + " " + thePull.getThird() + "\n");
+
+      if (winnings == 0)
+         System.out.println ("Losing statement… ");
+      else
+         System.out.println ("Winning statement: " + winnings);
    }
 
 
@@ -150,24 +159,24 @@ class TripleString
 { 
    private String string1, string2, string3;
    public static final int MAX_LEN = 20;
-   public static final int MAX_PULLS = 40;
+   public static final int MAX_PULLS = 5;
    public static int pullWinnings[] = new int[MAX_PULLS];
-   private int index = 0;
+   public static int index = 0;
 
    //Default Constructor
    public TripleString()
    {
       string1 = string2 = string3 = "";
    }
-   
+
    //Mutators
    public boolean setFirst(String firstSymbol)
    {
       if (validString(firstSymbol))
-         {
-            string1 = firstSymbol;
-            return true;
-         }
+      {
+         string1 = firstSymbol;
+         return true;
+      }
       else 
          return false;
    }
@@ -179,8 +188,8 @@ class TripleString
          string2 = secondSymbol;
          return true;
       }
-   else 
-      return false;
+      else 
+         return false;
    }
 
    public boolean setThird(String thirdSymbol)
@@ -190,29 +199,39 @@ class TripleString
          string3 = thirdSymbol;
          return true;
       }
-   else 
-      return false;
+      else 
+         return false;
    }
-   
+
    //Accessors
    public String getFirst()
    {
       return string1;
    }
-   
+
    public String getSecond()
    {
       return string2;
    }
-   
+
    public String getThird()
    {
       return string3;
    }
-   
+
    public String toString()
    {
       return getFirst() + getSecond() + getThird();
+   }
+   
+   public boolean continuePlay()
+   {
+      if (index == MAX_PULLS)
+      {
+         return false;
+      }
+      else
+         return true;
    }
    
    private boolean validString(String str)
@@ -224,7 +243,7 @@ class TripleString
       else
          return false;
    }
-   
+
    //Winning Methods
    public boolean saveWinnings(int winnings)
    {
@@ -237,17 +256,23 @@ class TripleString
       else
          return false;
    }
-   
+
    public String displayWinnings()
    {
       String allWinnings = "";
-      
-      for (int i = 0; i < MAX_PULLS; i++)
+
+      for (int i = 0; i < index; i++)
       {
          allWinnings += Integer.toString(pullWinnings[i]) + " ";
       }
-      
+
       return allWinnings;
    }
-   
+
 } 
+
+/***********************************OUTPUT******************************************
+
+
+*************************************************************************************/
+
